ed8828cd67a5d929998a3e6ace768eccc02620f6
Roman Khrystoforov <rah83000@gmail.com>
2025-12-09 10:55:59 +0100

    fixed config file line 6

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     TOKEN_EXPIRE_MINUTES: int = 30
     SECRET_KEY: str
-    DATABASE_URL: str | None = None
+    DATABASE_URL: str
 
     class Config:
         env_file = ".env"
